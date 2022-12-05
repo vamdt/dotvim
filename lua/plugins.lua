9521ca8a5c2207434608d9584b99641e05a153e7
vamdt <liang6688267@gmail.com>
2022-12-05 20:51:29 +0800

    Add packer

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'altercation/vim-colors-solarized'
+  use 'neoclide/coc.nvim'
+  use 'honza/vim-snippets'
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+  use 'junegunn/fzf.vim'
+  use 'fatih/vim-go'
+  use 'Shougo/defx.nvim'
+  use 'kristijanhusak/defx-icons'
+  use 'junegunn/vim-easy-align'
+  use 'ibhagwan/fzf-lua'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'numToStr/FTerm.nvim'
+end)
