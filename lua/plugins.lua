52a1ebfc8f70abffd277b046d89377eb1309b6f0
liang <liang@clbao.com>
2024-03-05 17:45:40 +0800

    Use lazy

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,33 @@
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'altercation/vim-colors-solarized'
-  use 'lifepillar/vim-solarized8'
-  
-  use { 'neoclide/coc.nvim', 
-        branch= 'release'
-  }
-  use 'jiangmiao/auto-pairs'
-  use 'honza/vim-snippets'
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
-  use 'Shougo/defx.nvim'
-  use 'kristijanhusak/defx-git'
-  use 'kristijanhusak/defx-icons'
-  use 't9md/vim-choosewin'
-  use 'fatih/vim-go'
-  use 'junegunn/vim-easy-align'
-  use 'ibhagwan/fzf-lua'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+return require('lazy').setup({
+  'altercation/vim-colors-solarized',
+  'lifepillar/vim-solarized8',
+  { 'neoclide/coc.nvim', branch= 'release' },
+   'jiangmiao/auto-pairs',
+   'honza/vim-snippets',
+   'tpope/vim-fugitive',
+   'airblade/vim-gitgutter',
+   'Shougo/defx.nvim',
+   'kristijanhusak/defx-git',
+   'kristijanhusak/defx-icons',
+   't9md/vim-choosewin',
+   'fatih/vim-go',
+   'junegunn/vim-easy-align',
+   'ibhagwan/fzf-lua',
  
-  use 'kyazdani42/nvim-web-devicons'
-  use 'numToStr/FTerm.nvim'
-  use {'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-  }
-end)
+   'kyazdani42/nvim-web-devicons',
+   'numToStr/FTerm.nvim',
+   {'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
+})
